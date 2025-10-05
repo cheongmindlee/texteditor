430df3549c2f511fd2aa9da4e9fdef95f3f3713c
David Lee <Cheongmin.gitlee@gmail.com>
2025-10-05 15:32:20 -0500

    Created variables inside SimpleString Buffer and finished the insert function

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -1,11 +1,34 @@
 package edu.grinnell.csc207.texteditor;
-
 /**
  * A naive implementation of a text buffer using a <code>String</code>.
  */
 public class SimpleStringBuffer {
+
+    //Holds the size of the text buffer
+    private int sz;
+    //Holds position of the text buffer
+    private int pos;
+
+    //Holds the built string.
+    private String retString;
+    /**
+     * Creates an empty SimpleStringBuffer with sz = 0
+     */
+    public SimpleStringBuffer(){
+        this.sz = 0;
+        this.pos = 0;
+        this.retString = "";
+    }
+
+    /**
+     * Inserts a character at the current position of the cursor, and advances the cursor one position forward
+     * @param ch The character we want to insert
+     */
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        String newString = retString.substring(0, pos) + ch + retString.substring(pos, sz);
+        pos++;
+        sz++;
+        retString = newString;
     }
 
     public void delete() {
